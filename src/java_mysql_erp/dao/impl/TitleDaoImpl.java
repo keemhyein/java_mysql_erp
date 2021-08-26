8ee311aa5d65bbb6fa51216b140e1396d6a16f07
hyeinkim <fairyhi@naver.com>
2021-08-26 17:42:11 +0900

    나머지 CRUD작업 완료~~!

--- src/java_mysql_erp/dao/impl/TitleDaoImpl.java
+++ src/java_mysql_erp/dao/impl/TitleDaoImpl.java
@@ -83,13 +83,32 @@ public class TitleDaoImpl implements TitleDao {
 
 	@Override
 	public int updateTitle(Title title) {
-		// TODO Auto-generated method stub
+		String sql = "update title set name = ? where code = ?";
+		try(Connection con = jdbcUtil.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setString(1, title.getName());
+			pstmt.setInt(2, title.getCode());
+			
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
 	@Override
 	public int deleteTitle(Title title) {
-		// TODO Auto-generated method stub
+		String sql = "delete from title where code = ?";
+		try(Connection con = jdbcUtil.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setInt(1, title.getCode());
+			
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
--- test/java_mysql_erp/dao/TitleDaoTest.java
+++ test/java_mysql_erp/dao/TitleDaoTest.java
@@ -1,19 +1,20 @@
 package java_mysql_erp.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java_mysql_erp.dao.impl.TitleDaoImpl;
 import java_mysql_erp.dto.Title;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TitleDaoTest {
 	private TitleDao dao;
 	
@@ -25,11 +26,12 @@ public class TitleDaoTest {
 
    @After
    public void tearDown() throws Exception {
+	   System.out.println();
 	   dao = null;
    }
 
    @Test
-   public void testSelectTitleByAll() {
+   public void test01SelectTitleByAll() {
       System.out.println("testSelectTitleByAll()");
       ArrayList<Title> list = dao.selectTitleByAll();
       Assert.assertNotEquals(0, list.size());
@@ -38,7 +40,7 @@ public class TitleDaoTest {
    }
 
    @Test
-   public void testSelectTiltleByCode() {
+   public void test02SelectTiltleByCode() {
 	  System.out.println("testSelectTiltleByCode()");
 	  Title selectTitle = dao.selectTitleByCode(new Title(5));
 	  Assert.assertNotNull(selectTitle);
@@ -46,7 +48,7 @@ public class TitleDaoTest {
    }
 
    @Test
-   public void testInsertTitle() {
+   public void test03InsertTitle() {
 	   System.out.println("testInsertTitle()");
 	   Title newTitle = new Title(6, "인턴");
 	   int res = dao.insertTitle(newTitle);
@@ -54,13 +56,25 @@ public class TitleDaoTest {
    }
 
    @Test
-   public void testUpdateTitle() {
-      fail("Not yet implemented");
+   public void test04UpdateTitle() {
+	   System.out.println("test04UpdateTitle()");
+	   //검색후 수정
+	   Title selectedTitle = dao.selectTitleByCode(new Title(6));
+	   selectedTitle.setName("계약직");
+	   
+	   int res = dao.updateTitle(selectedTitle);
+	   Assert.assertEquals(1, res);
+	   
+	   test01SelectTitleByAll();
    }
 
    @Test
-   public void testDeleteTitle() {
-      fail("Not yet implemented");
+   public void test05DeleteTitle() {
+	   System.out.println("test05DeleteTitle()");   
+	   int res = dao.deleteTitle(new Title(6));
+	   Assert.assertEquals(1, res);
+	   
+	   test01SelectTitleByAll();
    }
 
 }
